08619cdd3644f97a708f95671f061e329875676b
Alex <ag-dev@outlook.fr>
2019-07-22 16:25:41 +0200

    Initial commit

---
+++ File.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonGenomix
+{
+    public class File : IEnumerable<string>
+    {
+        private string _path;
+
+        public string Path
+        {
+            get { return _path; }
+            private set { _path = value; }
+        }
+
+        public File(string path) : this(path, false) { }
+        public File(string path, bool clean)
+        {
+            _path = path;
+            if (clean)
+            {
+                Clean();
+            }
+        }
+
+        public void Clean()
+        {
+            StreamWriter stream = new StreamWriter(_path, false);
+            stream.Close();
+        }
+        public string JsonString
+        {
+            get
+            {
+                string txt = "";
+                foreach (string line in this)
+                {
+                    txt += line + "\r\n";
+                }
+
+                return txt;
+
+            }
+            set
+            {
+                Clean();
+                this.Add(value);
+            }
+        }
+
+        public void Add(string line)
+        {
+            StreamWriter stream = new StreamWriter(_path, true);
+            stream.WriteLine(line);
+            stream.Close();
+        }
+
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return new FileEnumerator(_path);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+
+    class FileEnumerator : IEnumerator<string>
+    {
+        private StreamReader _stream;
+        private string _line;
+
+        public FileEnumerator(string name)
+        {
+            _stream = new StreamReader(name);
+        }
+
+        public string Current => _line;
+
+        object IEnumerator.Current => this.Current;
+
+        public void Dispose()
+        {
+            _stream.Close();
+        }
+
+        public bool MoveNext()
+        {
+            if (!_stream.EndOfStream)
+            {
+                _line = _stream.ReadLine();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            _stream.DiscardBufferedData();
+            _line = null;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JsonGenomix
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<File> files;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            files = new ObservableCollection<File>();
+            ListeViewFiles.ItemsSource = files;
+        }
+
+        private void SelectJson_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Multiselect = true;
+            fileDialog.Filter = "Json files(*.json)|*.json|All files(*.*)|*.*";
+            if (fileDialog.ShowDialog() == true)
+            {
+                foreach (string name in fileDialog.FileNames)
+                {
+                    files.Add(new File(name));
+                }
+
+            }
+        }
+        private void RemoveFileListe(object sender, RoutedEventArgs e)
+        {
+            File selected = (File)ListeViewFiles.SelectedItem;
+            files.Remove(selected);
+        }
+
+        private void DisplayContent(object sender, MouseButtonEventArgs e)
+        {
+            File selected = (File)ListeViewFiles.SelectedItem;
+            FileDisplay fileDisplay = new FileDisplay(selected);
+            fileDisplay.Show();
+        }
+    }
+}
